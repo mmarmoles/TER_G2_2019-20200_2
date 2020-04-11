45b31c0447bf28367e03f1b34e31eebdc5aa999f
Marc Mármol Escobar <mmarmoles@uoc.edu>
2020-04-11 18:09:03 +0200

    Modificación proyecto VISUAL STUDIO
    He creado un proyecto de VISUAL STUDIO (En su version Gratuida 2019), y he añadido dos archivos .ph uno CORE y otro TEST, en este punto son un clon, pero a partir de ahora solo usaré el TEST.

---
+++ Encriptador/Encriptador/TEST.py
@@ -0,0 +1,24 @@
+
+# VARIABLES 
+
+proceso = 0
+
+# FUNCIONES
+def validarBoolRutaInicial(proceso):
+    preguntaInicial = "F"
+    while proceso != 1:
+        if preguntaInicial == "S" or preguntaInicial == "s":
+                archivoEnDescargas = True
+                print("De acuerdo el archivo se encuentra en DESCARGAS \n")
+                proceso = 1
+        elif preguntaInicial == "N" or preguntaInicial == "n":
+              archivoEnDescargas = False
+              print("De acuerdo el archivo no se encuentra en DESCARGAS \n")
+              proceso = 1
+        else:
+            preguntaInicial = input("Por favor indicanos si el archivo se encuentra en DESCARGAS (S/N) \n")
+
+# PROGRAMA
+
+print("Bienvenido al Engriptador \n")
+validarBoolRutaInicial(proceso);

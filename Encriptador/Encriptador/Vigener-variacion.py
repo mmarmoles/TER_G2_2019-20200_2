fe6577aa91d89c69d0b294597f782f6a2bcd0037
ocugutiylla <63097180+ocugutiylla@users.noreply.github.com>
2020-04-14 13:04:40 +0200

    Test commite

---
+++ Encriptador/Encriptador/Vigener-variacion.py
@@ -0,0 +1,45 @@
+# VARIABLES
+abcdario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+letra_indice = dict(zip(abcdario, range(len(abcdario))))
+indice_letra = dict(zip(range(len(abcdario)), abcdario))
+
+# FUNCION DE ENCRYPTACION
+def encrypt(message, key):
+    encrypted = ''
+    split_message = [message [i:i + len (key)] for i in range(0, len(message), len(key))]
+
+    for each_split in split_message:
+        i = 0
+        for letter in each_split:
+            number = (letra_indice[letter] + letra_indice[key[i]]) % len(abcdario)
+            encrypted += indice_letra[number]
+            i+=1
+
+    return encrypted
+
+    pass
+
+ # FUNCION DE ENCRYPTACION
+def dencrypt(cipher, key):
+    decrypted = ''
+    split_cipher = [cipher [i:i + len (key)] for i in range(0, len(cipher), len(key))]
+    for each_split in split_cipher:
+        i = 0
+        for letter in each_split:
+            number = (letra_indice[letter] - letra_indice[key[i]]) % len(abcdario)
+            decrypted += indice_letra[number]
+            i+=1
+
+    return decrypted
+
+def main():
+    key = 'MARC'
+    message = 'ENCRIPTADO'
+    encrypted_message = encrypt(message, key)
+    dencrypted_message = dencrypt(encrypted_message, key)
+
+    print('Mensaje original:' + message)
+    print('Mensaje encyptado:' + encrypted_message)
+    print('Mensaje desencryptado:' + dencrypted_message)
+
+main()
